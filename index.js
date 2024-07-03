cbb9cf9eb2907100432f2cc8c1f0333b0f71dda6
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2024-07-03 11:27:01 +0530

    learned nodemon and custom middleware

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(middleware);
+app.use(logger);
 
+function middleware(req, res, next) {
+    console.log("called");
+    next();
+}
+
+function logger(req,res,next){
+    console.log(req.method,res.bowser,req.url);
+    next();
+}
+
+
+// Using the middleware
 let course = [
     {id: "1", name: "java"},
     {id: "2", name: "python"},
@@ -23,6 +37,20 @@ app.post("/courses",(req,res)=>{
     course.push(singleCourse);
     res.send(course);
 })
+
+app.put("/courses/:id",(req,res)=>{
+    let singleCourse = course.find(c => c.id === req.params.id);
+    singleCourse.name = req.body.name;
+    res.send(course);
+})
+app.delete("/courses/:id",(req,res)=>{
+    let singleCourse= course.find(c => c.id === req.params.id);
+    let index = course.indexOf(singleCourse);
+    course.splice(index,1);
+    res.send(course);
+})
+
+
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
